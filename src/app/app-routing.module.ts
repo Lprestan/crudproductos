dd787d14cf6726f75c2c3c93bd807075e5ee245b
larryprestan <larry.prestan@gmail.com>
2024-10-09 18:30:06 -0500

    navegacion links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NuevoproductoComponent } from './nuevoproducto/nuevoproducto.component';
 import { NavegacionComponent } from './navegacion/navegacion.component';
 import { EditarproductoComponent } from './editarproducto/editarproducto.component';
+import { ListadoproductosComponent } from './listadoproductos/listadoproductos.component';
 
 const routes: Routes = [
   {path:'',component:NuevoproductoComponent},
   {path:'nuevoproducto',component:NuevoproductoComponent},
   {path:'navegacion',component:NavegacionComponent},
+  {path:'listadoproductos',component:ListadoproductosComponent},
   {path:'editarproducto/:id',component:EditarproductoComponent}
 ];
 
