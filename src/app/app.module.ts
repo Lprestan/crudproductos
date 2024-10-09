f5f001c4c185fa21f9049e6ef77ec9b9b8d1aba1
larryprestan <larry.prestan@gmail.com>
2024-10-09 18:11:14 -0500

    navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NuevoproductoComponent } from './nuevoproducto/nuevoproducto.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NuevoproductoComponent
+    NuevoproductoComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
