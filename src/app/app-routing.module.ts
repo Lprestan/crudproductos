65d6f5c4368275542320815e9b9b1659c5e0614d
larryprestan <larry.prestan@gmail.com>
2024-10-09 18:14:37 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevoproductoComponent } from './nuevoproducto/nuevoproducto.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { EditarproductoComponent } from './editarproducto/editarproducto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:NuevoproductoComponent},
+  {path:'nuevoproducto',component:NuevoproductoComponent},
+  {path:'navegacion',component:NavegacionComponent},
+  {path:'editarproducto/:id',component:EditarproductoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
