170ea9ce9f6b938c17685c0fe4556c2374d62f63
larryprestan <larry.prestan@gmail.com>
2024-10-09 20:06:26 -0500

    nuevo producto

--- src/app/nuevoproducto/nuevoproducto.component.ts
+++ src/app/nuevoproducto/nuevoproducto.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class NuevoproductoComponent {
 
+  nombre:string;
+  precio:string;
+  detalle:string;
+  imagen:any;
+  ImgSeleccionada:any;
+  ImgMostrar:any;
+
+  constructor(){}
+
+  OtenerImagen(event:any){
+    this.ImgSeleccionada = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = e => this.ImgMostrar = reader.result;
+    reader.readAsDataURL(this.ImgSeleccionada);
+  }
+
 }
